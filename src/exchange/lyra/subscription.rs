d4fb7f458975d1d6c3c829f4f86395db49ccbb0e
Hju Kneyck Flores <hjucode@gmail.com>
2024-08-05 00:09:20 +0800

    Make `id` field optional and simplify validator implementation

--- src/exchange/lyra/subscription.rs
+++ src/exchange/lyra/subscription.rs
@@ -7,7 +7,7 @@ use serde::Serialize;
 
 #[derive(Clone, Eq, PartialEq, Debug, Deserialize, Serialize)]
 pub struct LyraSubResponse {
-    id: String,
+    id: Option<String>,
     result: Option<LyraSubResult>,
 }
 
@@ -22,32 +22,35 @@ impl Validator for LyraSubResponse {
     where
         Self: Sized,
     {
-        if let Some(result) = &self.result {
-            let failed_tickers: Vec<_> = result
-                .status
-                .iter()
-                .filter_map(|(key, value)| {
-                    if value != "ok" {
-                        Some((key.clone(), value.clone()))
-                    } else {
-                        None
-                    }
-                })
-                .collect();
+        // It should be safe to ignore failed subscription responses. Worst case scenario is that
+        // we won't receive any data for an invalid subscription
+        Ok(self)
+        // if let Some(result) = &self.result {
+        //     let failed_tickers: Vec<_> = result
+        //         .status
+        //         .iter()
+        //         .filter_map(|(key, value)| {
+        //             if value != "ok" {
+        //                 Some((key.clone(), value.clone()))
+        //             } else {
+        //                 None
+        //             }
+        //         })
+        //         .collect();
 
-            if failed_tickers.is_empty() {
-                Ok(self)
-            } else {
-                Err(SocketError::Subscribe(format!(
-                    "Received failure subscription response for tickers: {:?}",
-                    failed_tickers
-                )))
-            }
-        } else {
-            Err(SocketError::Subscribe(format!(
-                "Received failure subscription response: {:?}",
-                self
-            )))
-        }
+        //     if failed_tickers.is_empty() {
+        //         Ok(self)
+        //     } else {
+        //         Err(SocketError::Subscribe(format!(
+        //             "Received failure subscription response for tickers: {:?}",
+        //             failed_tickers
+        //         )))
+        //     }
+        // } else {
+        //     Err(SocketError::Subscribe(format!(
+        //         "Received failure subscription response: {:?}",
+        //         self
+        //     )))
+        // }
     }
 }

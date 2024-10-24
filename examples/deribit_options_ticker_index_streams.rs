5cf33a9146c88c4820de5b7d49150669bb2ff5a5
Hju Flores <hju@kodecraft.dev>
2024-10-25 07:03:35 +0800

    Update example ticker stream setup and add expiry handling
    - Remove redundant instrument data from coincall_ticker_streams.rs
    - Refactor deribit_options_ticker_index_streams.rs to improve ticker stream setup
    - Introduce expiry checker and close signal handling for graceful shutdown

--- examples/deribit_options_ticker_index_streams.rs
+++ examples/deribit_options_ticker_index_streams.rs
@@ -1,75 +1,155 @@
+use barter_data::event::MarketEvent;
 use barter_data::exchange::deribit::DeribitMain;
 use barter_data::streams::Streams;
-use barter_data::subscription::index::Indices;
+use barter_data::subscription::ticker::Ticker;
 use barter_data::subscription::ticker::Tickers;
-use barter_integration::model::instrument::kind::InstrumentKind;
-use barter_integration::model::instrument::kind::OptionContract;
-use barter_integration::model::instrument::kind::OptionExercise;
-use barter_integration::model::instrument::kind::OptionKind;
-use chrono::TimeZone;
-use chrono::Utc;
-use tokio::spawn;
-use tokio_stream::StreamExt;
+use barter_integration::model::instrument::kind::{
+    InstrumentKind, OptionContract, OptionExercise, OptionKind,
+};
+use chrono::{TimeZone, Utc};
+use futures::task::{Context, Poll};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use tokio::sync::{watch, Mutex};
+use tokio::time::{sleep, Duration as TokioDuration};
 use tracing::info;
 use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
-    // Initialise INFO Tracing log subscriber
     init_logging();
 
-    // Initialise Ticker streams for Deribit
-    let ticker_streams = Streams::<Tickers>::builder()
-        .subscribe([(
-            DeribitMain::default(),
-            "btc",
-            "usd",
-            InstrumentKind::Option(OptionContract {
-                kind: OptionKind::Call,
-                exercise: OptionExercise::American,
-                expiry: Utc.timestamp_millis_opt(1721980800000).unwrap(),
-                strike: rust_decimal_macros::dec!(65000),
-            }),
-            Tickers,
-        )])
-        .init()
-        .await
-        .unwrap();
+    // Set expiry timestamp for option contract
+    let expiry_timestamp = 1727966280000;
 
-    // Initialise Index streams for Deribit
-    let index_streams = Streams::<Indices>::builder()
-        .subscribe([(
-            DeribitMain::default(),
-            "btc",
-            "usdc",
-            InstrumentKind::Spot,
-            Indices,
-        )])
-        .init()
-        .await
-        .unwrap();
+    // Configure and subscribe to Deribit options stream
+    let ticker_stream = setup_ticker_stream(expiry_timestamp).await;
 
-    // Join ticker streams
-    let mut joined_ticker_stream = ticker_streams.join_map().await;
+    // Create watch channel for the close signal
+    let (close_tx, close_rx) = watch::channel(false);
+    let close_signal = Arc::new(AtomicBool::new(false));
 
-    // Join index streams
-    let mut joined_index_stream = index_streams.join_map().await;
+    // Start tasks to handle the expiry check and close signal
+    let ticker_stream: Arc<Mutex<_>> = Arc::new(Mutex::new(ticker_stream));
 
-    // Spawn a task to process Ticker data
-    spawn(async move {
-        while let Some((exchange, ticker)) = joined_ticker_stream.next().await {
-            info!("Exchange: {exchange}, TickerData: {ticker:?}");
+    spawn_expiry_checker(expiry_timestamp, close_tx);
+    spawn_close_signal_handler(
+        Arc::clone(&ticker_stream),
+        close_rx.clone(),
+        Arc::clone(&close_signal),
+    );
+
+    // Process ticker data
+    process_ticker_data(ticker_stream, close_signal, close_rx).await;
+
+    info!("Market data streaming task exited.");
+}
+
+/// Initializes the ticker stream and sets up the subscription to Deribit options
+async fn setup_ticker_stream(
+    expiry_timestamp: i64,
+) -> tokio::sync::mpsc::UnboundedReceiver<MarketEvent<Ticker>> {
+    let stream_builder = Streams::<Tickers>::builder();
+    let builder = stream_builder.subscribe([(
+        DeribitMain::default(),
+        "btc",
+        "usd",
+        InstrumentKind::Option(OptionContract {
+            kind: OptionKind::Call,
+            exercise: OptionExercise::American,
+            expiry: Utc.timestamp_millis_opt(expiry_timestamp).unwrap(),
+            strike: rust_decimal_macros::dec!(65000),
+        }),
+        Tickers,
+    )]);
+
+    builder.init().await.unwrap().join().await
+}
+
+/// Spawns a task to check the expiry and send a close signal if expired
+fn spawn_expiry_checker(expiry_timestamp: i64, close_tx: watch::Sender<bool>) {
+    tokio::spawn(async move {
+        let current_timestamp = Utc::now().timestamp_millis();
+        if current_timestamp >= expiry_timestamp {
+            info!(
+                "Option contract already expired. Timestamp: {}",
+                expiry_timestamp
+            );
+            close_tx.send(true).unwrap();
+            return;
+        }
+
+        loop {
+            let current_timestamp = Utc::now().timestamp_millis();
+            if current_timestamp >= expiry_timestamp {
+                info!(
+                    "Option contract has expired. Timestamp: {}",
+                    expiry_timestamp
+                );
+                close_tx.send(true).unwrap();
+                break;
+            }
+
+            sleep(TokioDuration::from_secs(1)).await;
         }
     });
+}
 
-    // Process Index data
-    while let Some((exchange, index)) = joined_index_stream.next().await {
-        info!("Exchange: {exchange}, IndexData: {index:?}");
+/// Spawns a task to handle the close signal and close the ticker stream gracefully
+fn spawn_close_signal_handler(
+    ticker_stream: Arc<Mutex<tokio::sync::mpsc::UnboundedReceiver<MarketEvent<Ticker>>>>,
+    mut close_rx: watch::Receiver<bool>,
+    close_signal: Arc<AtomicBool>,
+) {
+    tokio::spawn(async move {
+        while close_rx.changed().await.is_ok() {
+            if *close_rx.borrow() {
+                info!("Received close signal. Closing the ticker stream.");
+                close_signal.store(true, Ordering::SeqCst);
+
+                let mut ticker_stream = ticker_stream.lock().await;
+                info!("Invoking close() on the ticker stream");
+                ticker_stream.close();
+                info!("Market event stream closed.");
+                break;
+            }
+        }
+    });
+}
+
+/// Processes ticker data and exits when the stream is closed
+async fn process_ticker_data(
+    ticker_stream: Arc<Mutex<tokio::sync::mpsc::UnboundedReceiver<MarketEvent<Ticker>>>>,
+    close_signal: Arc<AtomicBool>,
+    close_rx: watch::Receiver<bool>,
+) {
+    while !close_signal.load(Ordering::SeqCst) {
+        let mut ticker_stream = ticker_stream.lock().await;
+
+        // Check for new close signal changes in the loop
+        if close_rx.has_changed().unwrap_or(false) && *close_rx.borrow() {
+            info!("Close signal received, breaking out of the data processing loop.");
+            break;
+        }
+
+        match ticker_stream.poll_recv(&mut Context::from_waker(futures::task::noop_waker_ref())) {
+            Poll::Ready(Some(data)) => {
+                info!("TickerData: {data:?}");
+            }
+            Poll::Ready(None) => {
+                info!("Ticker stream has closed, exiting loop.");
+                break;
+            }
+            Poll::Pending => {
+                drop(ticker_stream);
+            }
+        }
     }
 }
 
-// Initialise an INFO `Subscriber` for `Tracing` logs and install it as the
-// global default.
+/// Initializes logging with the `tracing` crate
 fn init_logging() {
     tracing_subscriber::fmt()
         .with_env_filter(
